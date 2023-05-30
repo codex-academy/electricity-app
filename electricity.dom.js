f5503b57269c4bb0d8e4fb27901a7008697b5b27
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-05-30 15:58:37 +0200

    Dom halfway working half of the tests passing

--- electricity.dom.js
+++ electricity.dom.js
@@ -13,13 +13,43 @@ const electricity =  Electricity();
 
 // DOM events here 
 function linkBuyToDOM(){
-    var buyRadioBtn = document.querySelector("input[name='buyElectricity']");
+    var buyRadioBtn = document.querySelector("input[name='buyElectricity']:checked");
 
-    for(var i=0; i<buyRadioBtn.length; i++){
-        if(buyRadioBtn[i].checked && buyRadioBtn[i].value == '10')
-        electricity.topUpElectricity("10")
+console.log(buyRadioBtn.value)
+    if(buyRadioBtn && buyRadioBtn.value == '10'){
+        electricity.topUpElectricity('10')
         unitsAvailableElement.innerHTML = electricity.getUnitsAvailable()
+        totalAmountSpentElement.innerHTML = electricity.totalAmountSpent()
+        totalUnitsBoughtElement.innerHTML = electricity.totalUnitsBought()
     }
-    
+    if(buyRadioBtn && buyRadioBtn.value == '20'){
+        electricity.topUpElectricity('20')
+        unitsAvailableElement.innerHTML = electricity.getUnitsAvailable()
+        totalAmountSpentElement.innerHTML = electricity.totalAmountSpent()
+        totalUnitsBoughtElement.innerHTML = electricity.totalUnitsBought()
+    }
+    if(buyRadioBtn && buyRadioBtn.value == '50'){
+        electricity.topUpElectricity('50')
+        unitsAvailableElement.innerHTML = electricity.getUnitsAvailable()
+        totalAmountSpentElement.innerHTML = electricity.totalAmountSpent()
+        totalUnitsBoughtElement.innerHTML = electricity.totalUnitsBought()
+    }
+    if(buyRadioBtn && buyRadioBtn.value == 'advance'){
+        electricity.topUpElectricity('advance')
+        unitsAvailableElement.innerHTML = electricity.getUnitsAvailable()
+        advanceTakenElement.style.display = "block"
+        totalUnitsBoughtElement.innerHTML = electricity.totalUnitsBought()
+    }
 
-}buyBtnElement.addEventListener("click", linkBuyToDOM)
+}
+buyBtnElement.addEventListener("click", linkBuyToDOM)
+
+function linkUseApplianceToDOM(){
+    var useRadioBtn = document.querySelector("input[name='useElectricity']:checked");
+
+    if(useRadioBtn){
+        electricity.useAppliance(useRadioBtn.value)
+       unitsAvailableElement.innerHTML = electricity.getUnitsAvailable()
+    }
+}
+useBtnElement.addEventListener("click", linkUseApplianceToDOM)
--- electricity.js
+++ electricity.js
@@ -11,64 +11,79 @@ function Electricity() {
    let unitsAvailable = 0
    let globalVariable1;
    let globalVariable2;
-   //let units = 0
+   let totalUnits =0;
+
    let advanceTotal = 0
    let moneyOwed = 0
    let totalAmount = 0
 
     function topUpElectricity(amount) {
+        //check if the amount is equal to 10
         if(amount == "10"){
-            //units = 7
+        //if it is check if the moneyOwed variable is less than zero  
             if(moneyOwed<0){
+                //if it is add 10 to moneyOwed
                 moneyOwed +=10
                 
             }
+            //check if money owed is greater than zero
             if(moneyOwed >= 0){
+                //if it is add 10 to total amount and 7 to units
                 totalAmount +=10
-                unitsAvailable = moneyOwed+=7
+                unitsAvailable +=7
+                totalUnits +=7
             }
         
         }
+        //check if the amount is equal to 20
         else if(amount == "20"){
-           // units = 14
+           // if it is check if moneyOwed is less than zero
            if(moneyOwed<0){
+            //it it is add 20 to money owed
                 moneyOwed +=20
                 
             }
-
+            //if it is not and it is greater than 0 add 20 to totalAMount and 14 to units
            if(moneyOwed >= 0){
             totalAmount +=20
-            unitsAvailable = moneyOwed+=14
+            unitsAvailable +=14
+            totalUnits +=14
            }
             
         }
+        //check if amount is equal to 50
         else if(amount == "50"){
+            //if it is check if money owed is less than zero
            if(moneyOwed<0){
+            //if it is add 50 to moneyOwed
                 moneyOwed +=50
-                
-                
+                 
             } 
-            // units = 35
+            //if moneyOwed is greater than 0 add 50 to amount and 35 to units
            if(moneyOwed >= 0){
             totalAmount +=50
-            unitsAvailable = moneyOwed += 35
-            //unitsAvailable +=35
+            unitsAvailable += 35
+            totalUnits +=35 
            }
            
         }
+        //check if amount is equal to advance 
         else if (amount == "advance"){
-            //units  = 21
+            //if it is check if money owed is less than 0
             if(moneyOwed < 0){
+                //if it is do not allow advance
                 unitsAvailable += 0
                 moneyOwed -= 0
                 advanceTotal = 0
             }
+            //check if money owed is greater than 0 if it is allow advance
             if(moneyOwed >= 0 ){
                 unitsAvailable += 21
                 moneyOwed -= 30
                 advanceTotal = 21 
+                totalUnits +=21
             }
-           
+           //make advance total a global variable
             globalVariable1 = advanceTotal
         }
         else{
@@ -78,6 +93,7 @@ function Electricity() {
     }
 
     function getUnitsAvailable() {
+        //make units available a global variable
         globalVariable2 = unitsAvailable
          return unitsAvailable;
     }
@@ -87,23 +103,18 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
-       if(appliance == appliances["Stove"]){
-        globalVariable2 - 10
+        //check if units available are greater than 10
+        if(appliance == "stove"){
+        if(globalVariable2>10){
+        globalVariable2 -= appliances["Stove"]
        }
-       if(appliance == appliances["Kettle"]){
-        globalVariable2 - 5
-       }
-       if(appliance == appliances["TV"]){
-        globalVariable2 - 3
-       }
-       if(appliance == appliances["Fridge"]){
-        globalVariable2 - 13
-       }
-       
-        
+     //  console.log(globalVariable2)
+       console.log(getUnitsAvailable())
     }
+}
 
     function advanceTaken() {
+        //check if the advance variable is and return true else if it is not return false
         if(globalVariable1){
             return true
         }
@@ -113,10 +124,13 @@ function Electricity() {
     }
 
     function totalAmountSpent() {
+        //return total amount
         return totalAmount
     }
 
     function totalUnitsBought(){
+        //return total units
+        return totalUnits
     }
 
     return {
