ff056635631533680e065b40f5644402b37e100d
NgomsoGaya <ngomso.gaya09@gmail.com>
2023-05-30 14:11:52 +0200

    a few tests are still failing

--- electricity.dom.js
+++ electricity.dom.js
@@ -1,9 +1,25 @@
 // DOM element(s) references
-
-
+const topUpRadioBtnElement = document.querySelector(".topup")
+const buyBtnElement = document.querySelector(".topupNow")
+const unitsAvailableElement = document.querySelector(".unitsAvailable")
+const totalUnitsBoughtElement = document.querySelector(".totalUnits")
+const totalAmountSpentElement = document.querySelector(".totalAmount")
+const advanceTakenElement = document.querySelector(".advanceTaken")
+const applianceRadioBtnElement = document.querySelector(".usage")
+const useBtnElement = document.querySelector(".useNow")
 
 // Factory Function instance 
 const electricity =  Electricity();
 
-
 // DOM events here 
+function linkBuyToDOM(){
+    var buyRadioBtn = document.querySelector("input[name='buyElectricity']");
+
+    for(var i=0; i<buyRadioBtn.length; i++){
+        if(buyRadioBtn[i].checked && buyRadioBtn[i].value == '10')
+        electricity.topUpElectricity("10")
+        unitsAvailableElement.innerHTML = electricity.getUnitsAvailable()
+    }
+    
+
+}buyBtnElement.addEventListener("click", linkBuyToDOM)
--- electricity.js
+++ electricity.js
@@ -8,12 +8,77 @@ function Electricity() {
         'Fridge': 13
     };
 
+   let unitsAvailable = 0
+   let globalVariable1;
+   let globalVariable2;
+   //let units = 0
+   let advanceTotal = 0
+   let moneyOwed = 0
+   let totalAmount = 0
+
     function topUpElectricity(amount) {
+        if(amount == "10"){
+            //units = 7
+            if(moneyOwed<0){
+                moneyOwed +=10
+                
+            }
+            if(moneyOwed >= 0){
+                totalAmount +=10
+                unitsAvailable = moneyOwed+=7
+            }
+        
+        }
+        else if(amount == "20"){
+           // units = 14
+           if(moneyOwed<0){
+                moneyOwed +=20
+                
+            }
 
+           if(moneyOwed >= 0){
+            totalAmount +=20
+            unitsAvailable = moneyOwed+=14
+           }
+            
+        }
+        else if(amount == "50"){
+           if(moneyOwed<0){
+                moneyOwed +=50
+                
+                
+            } 
+            // units = 35
+           if(moneyOwed >= 0){
+            totalAmount +=50
+            unitsAvailable = moneyOwed += 35
+            //unitsAvailable +=35
+           }
+           
+        }
+        else if (amount == "advance"){
+            //units  = 21
+            if(moneyOwed < 0){
+                unitsAvailable += 0
+                moneyOwed -= 0
+                advanceTotal = 0
+            }
+            if(moneyOwed >= 0 ){
+                unitsAvailable += 21
+                moneyOwed -= 30
+                advanceTotal = 21 
+            }
+           
+            globalVariable1 = advanceTotal
+        }
+        else{
+            unitsAvailable = 0 }
 
+            //return units
     }
 
     function getUnitsAvailable() {
+        globalVariable2 = unitsAvailable
          return unitsAvailable;
     }
 
@@ -22,13 +87,33 @@ function Electricity() {
     * other wise return false and do nothing.
     */
     function useAppliance(appliance) {
+       if(appliance == appliances["Stove"]){
+        globalVariable2 - 10
+       }
+       if(appliance == appliances["Kettle"]){
+        globalVariable2 - 5
+       }
+       if(appliance == appliances["TV"]){
+        globalVariable2 - 3
+       }
+       if(appliance == appliances["Fridge"]){
+        globalVariable2 - 13
+       }
+       
         
     }
 
     function advanceTaken() {
+        if(globalVariable1){
+            return true
+        }
+        else if(!globalVariable1){
+            return false
+        }
     }
 
     function totalAmountSpent() {
+        return totalAmount
     }
 
     function totalUnitsBought(){
